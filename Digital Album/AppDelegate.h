43ef05ef5c4a5e56f80ce1234730c2154d4a6ca9
Ernesto Carrión <ernesto@kogimobile.com>
2014-02-17 09:20:26 -0500

    Initial Import

---
+++ Digital Album/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Digital Album
+//
+//  Created by Ernesto Carrion on 2/17/14.
+//  Copyright (c) 2014 Salarion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Digital AlbumTests/Digital_AlbumTests.m
@@ -0,0 +1,34 @@
+//
+//  Digital_AlbumTests.m
+//  Digital AlbumTests
+//
+//  Created by Ernesto Carrion on 2/17/14.
+//  Copyright (c) 2014 Salarion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Digital_AlbumTests : XCTestCase
+
+@end
+
+@implementation Digital_AlbumTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
