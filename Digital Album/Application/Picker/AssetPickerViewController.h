ea1bbe37472e2086b729b563d34405b07824a8e2
Ernesto Carrión <ernesto@kogimobile.com>
2014-02-18 17:52:58 -0500

    Empty controller for asset picker

---
+++ Digital Album/Application/Picker/AssetPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AssetPickerViewController.h
+//  Digital Album
+//
+//  Created by Ernesto Carrion on 2/18/14.
+//  Copyright (c) 2014 Salarion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssetPickerViewController : UIViewController
+
+@end
---
+++ Digital Album/Application/Picker/AssetPickerViewController.m
@@ -0,0 +1,38 @@
+//
+//  AssetPickerViewController.m
+//  Digital Album
+//
+//  Created by Ernesto Carrion on 2/18/14.
+//  Copyright (c) 2014 Salarion. All rights reserved.
+//
+
+#import "AssetPickerViewController.h"
+
+@interface AssetPickerViewController ()
+
+@end
+
+@implementation AssetPickerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
